69c35975a283aeeda84dab63896a04953ed923c3
UniKroLL <s.chagin@yandex.ru>
2021-03-15 17:50:06 +0500

    feat: release func min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array == undefined || array.length == 0){return 0};
+
+  let min = 0;
+  let curr = 0;
+
+  for (let i=0; i<array.length; i++){
+      curr = array[i];
+      min = curr < min ? curr : min;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0){return 0};
+    let max = 0;
+    let curr = 0;
+    console.log(array);
+    for (let i=0; i<array.length; i++){
+        curr = array[i];
+        max = curr > max ? curr : max;
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0){return 0};
+    let avg = 0;
+    let arl = array.length;
+    let summ = 0;
+    console.log(array);
+    for (let i=0; i<arl; i++){
+        summ = summ+array[i];
+        avg = summ/arl;
+    }
+    return avg;
 }
